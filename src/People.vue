d9fe474d8b3eacf45f74f29539aec7cee7d2f63b
Kyle Stevenson <kylewstevenson225@gmail.com>
2021-06-03 14:34:06 -0400

    Added currently nonfunctional edit buttons to each Person's row.

--- src/People.vue
+++ src/People.vue
@@ -1,18 +1,26 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group">
-            <li
-                class="list-group-item"
+            <li class="list-group-item"
                 v-for="index in 5"
                 :key="index">
+
                 Person #{{ index }}
+                <button style="float:right;" @click="editPerson(index)">Edit</button>
+
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-
+    export default {
+        methods: {
+            editPerson(index) {
+                alert("Editing Person #" + index);
+            }
+        }
+    }
 </script>
 
 <style>
