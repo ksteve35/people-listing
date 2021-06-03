56381b9a4bf3761e9bac4886bb29d20914f35009
Kyle Stevenson <kylewstevenson225@gmail.com>
2021-06-03 14:22:50 -0400

    Created components to further build upon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <div id="app">
-    
+  <div class="container">
+    <app-header></app-header> 
+    <hr>
+    <div class="row">
+      <app-people></app-people>
+      <app-person-details></app-person-details>
+    </div>
   </div>
 </template>
 
 <script>
-  
+    import Header from './Header.vue';
+    import People from './People.vue';
+    import PersonDetails from './PersonDetails.vue';
+
+    export default {
+      data() {
+        return {
+          
+        };
+      },
+      components: {
+          'app-header': Header,
+          'app-people': People,
+          'app-person-details': PersonDetails
+      }
+    }
 </script>
 
 <style>
---
+++ src/PersonDetails.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p>Person Details will be updated here!</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
